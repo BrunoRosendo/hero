f49ed3a25c957e06695bf088aed7b7f93782bca8
Bruno Rosendo <brunomrosendo@hotmail.com>
2021-02-26 15:56:14 +0000

    Painted the floor

--- src/main/java/Arena.java
+++ src/main/java/Arena.java
@@ -1,5 +1,10 @@
 import javax.swing.*;
+
+import com.googlecode.lanterna.TerminalPosition;
+import com.googlecode.lanterna.TerminalSize;
 import com.googlecode.lanterna.TextCharacter;
+import com.googlecode.lanterna.TextColor;
+import com.googlecode.lanterna.graphics.TextGraphics;
 import com.googlecode.lanterna.input.KeyType;
 import com.googlecode.lanterna.input.KeyStroke;
 import com.googlecode.lanterna.screen.Screen;
@@ -46,7 +51,9 @@ public class Arena {
             hero.setPosition(position);
     }
 
-    public void draw(Screen screen) {
-        this.hero.draw(screen);
+    public void draw(TextGraphics graphics) {
+        graphics.setBackgroundColor(TextColor.Factory.fromString("#336699"));
+        graphics.fillRectangle(new TerminalPosition(0, 0), new TerminalSize(width, height), ' ');
+        this.hero.draw(graphics);
     }
 }
